a2f0f36f5c13cdea88b89b031f27705ee66f2408
repl.it user <replituser@example.com>
2021-01-17 20:13:35 +0000

    sodoku project

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,11 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
-
+      return res.json(solver.checkPlacement(req.body.puzzle, req.body.coordinate, req.body.value))
     });
     
   app.route('/api/solve')
     .post((req, res) => {
-
+      return res.json(solver.solve(req.body.puzzle, 0, 0));
     });
 };
